eb32372dc753e93818905d1d6cfbe01cbe999948
voroninPA <vibber25@gmail.com>
2025-06-11 13:38:55 +0300

    better

--- calculator.py
+++ calculator.py
@@ -1,51 +1,56 @@
 import operator
 import re
 
+
 class RPNAPIError(Exception):
     pass
+
 
 class InvalidTokenError(RPNAPIError):
     pass
 
+
 class MismatchedParenthesesError(RPNAPIError):
     pass
+
 
 class RPNSyntaxError(RPNAPIError):
     pass
 
+
 class RPNCalculator:
-    
+
     OPERATORS = {
         '+': operator.add,
         '-': operator.sub,
         '*': operator.mul,
         '/': operator.truediv,
     }
-    
+
     PRIORITY = {
         '+': 1,
         '-': 1,
         '*': 2,
         '/': 2,
     }
-    
+
     def __init__(self):
         self._functions = {
             'sin': lambda x: self.evaluate(f'0 {x} - sin'),
             'cos': lambda x: self.evaluate(f'0 {x} - cos'),
         }
-    
+
     def _tokenize(self, expression: str) -> list:
         tokens = re.findall(r'\d+\.?\d*|[()+*\/-]|\w+', expression)
         return tokens
-    
+
     def _validate_tokens(self, tokens: list) -> None:
         for token in tokens:
-            if not (token.replace('.', '').isdigit() or 
-                    token in self.OPERATORS or 
+            if not (token.replace('.', '').isdigit() or
+                    token in self.OPERATORS or
                     token in '()'):
                 raise InvalidTokenError(f'Недопустимый токен: {token}')
-    
+
     def _check_parentheses(self, tokens: list) -> None:
         stack = []
         for token in tokens:
@@ -53,19 +58,20 @@ class RPNCalculator:
                 stack.append(token)
             elif token == ')':
                 if not stack:
-                    raise MismatchedParenthesesError("Несбалансированные скобки")
+                    raise MismatchedParenthesesError(
+                        "Несбалансированные скобки")
                 stack.pop()
         if stack:
             raise MismatchedParenthesesError("Несбалансированные скобки")
-    
+
     def infix_to_rpn(self, expression: str) -> list:
         tokens = self._tokenize(expression)
         self._validate_tokens(tokens)
         self._check_parentheses(tokens)
-        
+
         output = []
         operators = []
-        
+
         for token in tokens:
             if token.replace('.', '').isdigit():
                 output.append(token)
@@ -80,21 +86,22 @@ class RPNCalculator:
                 while operators[-1] != '(':
                     output.append(operators.pop())
                 operators.pop()
-        
+
         while operators:
             output.append(operators.pop())
-        
+
         return output
-    
+
     def evaluate_rpn(self, rpn: list) -> float:
         stack = []
-        
+
         for token in rpn:
             if token.replace('.', '').isdigit():
                 stack.append(float(token))
             elif token in self.OPERATORS:
                 if len(stack) < 2:
-                    raise RPNSyntaxError("Недостаточно операндов для оператора")
+                    raise RPNSyntaxError(
+                        "Недостаточно операндов для оператора")
                 b = stack.pop()
                 a = stack.pop()
                 try:
@@ -104,12 +111,12 @@ class RPNCalculator:
                 stack.append(result)
             else:
                 raise InvalidTokenError(f'Недопустимый токен в ОПН: {token}')
-        
+
         if len(stack) != 1:
             raise RPNSyntaxError("Некорректное выражение")
-        
+
         return stack[0]
-    
+
     def evaluate(self, expression: str) -> float:
         rpn = self.infix_to_rpn(expression)
         return self.evaluate_rpn(rpn)
@@ -118,7 +125,7 @@ class RPNCalculator:
 if __name__ == '__main__':
     calc = RPNCalculator()
     print("Калькулятор ОПН. Введите 'quit' для выхода.")
-    
+
     while True:
         try:
             expr = input("Введите выражение: ").strip()
@@ -126,7 +133,7 @@ if __name__ == '__main__':
                 break
             if not expr:
                 continue
-                
+
             rpn = calc.infix_to_rpn(expr)
             result = calc.evaluate_rpn(rpn)
             print(f"ОПН: {' '.join(map(str, rpn))}")
--- test_calculator.py
+++ test_calculator.py
@@ -1,26 +1,28 @@
 import unittest
 from calculator import RPNCalculator, RPNAPIError, InvalidTokenError, MismatchedParenthesesError, RPNSyntaxError
 
+
 class TestRPNCalculator(unittest.TestCase):
     def setUp(self):
         self.calc = RPNCalculator()
-    
+
     def test_infix_to_rpn(self):
         test_cases = [
             ("3 + 4", ['3', '4', '+']),
             ("3 + 4 * 2", ['3', '4', '2', '*', '+']),
             ("(3 + 4) * 2", ['3', '4', '+', '2', '*']),
-            ("3 + 4 * 2 / (1 - 5)", ['3', '4', '2', '*', '1', '5', '-', '/', '+']),
+            ("3 + 4 * 2 / (1 - 5)", ['3', '4',
+             '2', '*', '1', '5', '-', '/', '+']),
             ("2 + 3 * 4 - 5", ['2', '3', '4', '*', '+', '5', '-']),
             ("(1 + 2) * (3 + 4)", ['1', '2', '+', '3', '4', '+', '*']),
             ("10 - (2 + 3) * 4", ['10', '2', '3', '+', '4', '*', '-']),
             ("2.5 + 3.7 * 4", ['2.5', '3.7', '4', '*', '+']),
         ]
-        
+
         for expr, expected in test_cases:
             with self.subTest(expr=expr):
                 self.assertEqual(self.calc.infix_to_rpn(expr), expected)
-    
+
     def test_evaluate_rpn(self):
         test_cases = [
             (['3', '4', '+'], 7),
@@ -28,11 +30,11 @@ class TestRPNCalculator(unittest.TestCase):
             (['3', '4', '+', '2', '*'], 14),
             (['3', '4', '2', '*', '1', '5', '-', '/', '+'], 1),
         ]
-        
+
         for rpn, expected in test_cases:
             with self.subTest(rpn=rpn):
                 self.assertAlmostEqual(self.calc.evaluate_rpn(rpn), expected)
-    
+
     def test_evaluate(self):
         test_cases = [
             ("3 + 4", 7),
@@ -42,21 +44,21 @@ class TestRPNCalculator(unittest.TestCase):
             ("10 / 2", 5),
             ("2.5 * 3", 7.5),
         ]
-        
+
         for expr, expected in test_cases:
             with self.subTest(expr=expr):
                 self.assertAlmostEqual(self.calc.evaluate(expr), expected)
-    
+
     def test_invalid_tokens(self):
         with self.assertRaises(InvalidTokenError):
             self.calc.evaluate("3 + 4a")
-    
+
     def test_mismatched_parentheses(self):
         with self.assertRaises(MismatchedParenthesesError):
             self.calc.evaluate("(3 + 4")
         with self.assertRaises(MismatchedParenthesesError):
             self.calc.evaluate("3 + 4)")
-    
+
     def test_syntax_errors(self):
         with self.assertRaises(RPNSyntaxError):
             self.calc.evaluate("3 +")
@@ -64,10 +66,11 @@ class TestRPNCalculator(unittest.TestCase):
             self.calc.evaluate("+ 3")
         with self.assertRaises(RPNSyntaxError):
             self.calc.evaluate("3 3 +")
-    
+
     def test_division_by_zero(self):
         with self.assertRaises(RPNSyntaxError):
             self.calc.evaluate("3 / 0")
+
 
 if __name__ == '__main__':
     unittest.main()
